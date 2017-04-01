6afd62ebc2f1c46298fd7503e914e3a06c14fb79
yx_chai@whu.edu.cn <yx_chai@whu.edu.cn>
2017-04-01 23:56:48 +0800

    Clean some code

--- BE_predictor_v1_2.m
+++ BE_predictor_v1_2.m
@@ -3,32 +3,6 @@ function [nn, loo_error] = BE_predictor_v1_2(X_original, Y_original, breakpoints
 % error_ave = BE_predictor(breakpoints, range, ratio_train, units)
 % by Yunxuan Chai(yx_chai@whu.edu.cn), 2017.3.12
 
-% BE_predictor_v1.0
-% Add divide func: Change the way that data is divided, from continuous to
-% discrete.
-% Change the calculation of ave_error to make it more reasonable.
-% by yx_chai, 2017.3.18
-
-% BE_predictor_v1.1
-% Change the way that data is divided, from 3 segments to 5, and more.
-% Add two boolean input 'coordinates' and 'root', where coordinates refer
-% to the energy coordinate of input, and root refers to whether apply
-% sqrt() to the input data.
-% by yx_chai, 2017.3.29
-
-% BE_predictor_v1.1.1
-% Change the parameter of fitnet by network's property based in MATLAB.
-% by yx_chai, 2017.3.30
-
-% BE_predictor_v1.2(LOO version)
-% Add multielements feature, change the source of data to satisfy the need
-% of multielements training.
-% Add LOO feature to the function, enable Leave-One-Out cross validation.
-% And the ratio_train feature is kept to enable the test set, test set will
-% be replaced by loo_sample
-% Bug fixed: in error_re_ave, put the abs to the Y_output step
-% by yx_chai, 2017.4.1
-
 %% Data preparation
 % clc
 % clear
